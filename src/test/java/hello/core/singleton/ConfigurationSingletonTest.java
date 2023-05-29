c395e840ce731d3b800406cbd384fed361c16b78
Youngmoon Oh <moony_bb@naver.com>
2023-05-29 15:14:34 +0900

    컴포넌트 스캔과 의존관계 자동 주입

--- src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
+++ src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -42,7 +42,6 @@ public class ConfigurationSingletonTest {
         // 이것은 내가 만든 클래스가 아니라 스프링이 CGLIB라는 바이트코드 조작 라이브러리를 사용해서
         // AppConfig 클래스를 상속받은 임의의 다른 클래스를 만들고, 그 다른 클래스를 스프링 빈으로 등록한 것이다.
         System.out.println("bean.getClass() = " + bean.getClass());
-
     }
 
 }
